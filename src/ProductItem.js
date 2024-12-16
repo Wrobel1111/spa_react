37e73517f2e116ec151b478c9639eb0452750f9b
Kacper Konarzewski <konarzewski32@gmail.com>
2024-12-16 11:22:23 +0100

    Cw 1 z 8

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -1,4 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
 function ProductItem(product)
 {
-	return (<li></li>)
+	var [products, setProducts] = useState([]);
+	useEffect(() =>
+	{
+		setProducts([
+			{
+				"id": 1,
+				"title": "iPhone 14",
+				"brand": "Apple"
+			},
+			{
+				"id": 2,
+				"title": "iPad Air",
+				"brand": "Apple"
+			},
+			{
+				"id": 3,
+				"title": "Galaxy A51",
+				"brand": "Samsung"
+			}
+		])
+	}, []);
+	return (<div><h1>List of products</h1><ul>{products.map((item, key) => {return <li key={key}>{item}</li>})}</ul></div>)
 }
+export default ProductItem;
